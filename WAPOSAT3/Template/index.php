61cdcd9206b15330fe5f611b64032b86c5ccfc97
JOTAI <juan.basflo.initec@gmail.com>
2015-01-30 09:33:35 -0500

    ultimos cambios a la plataforma, e inicio del diseño de la plataforma nueva

---
+++ WAPOSAT3/Template/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <title>WAPOSAT</title>
+    <?php  include_once("../include/head.default.php");?>
+    <!-- Especific CSS -->
+    <link href="index/style.index.css" rel="stylesheet">
+    
+</head>
+
+<body>
+    
+    <!-- Navigation -->
+    <?php include_once("../include/navbar.default.php"); ?>
+
+</body>
+
+</html>
